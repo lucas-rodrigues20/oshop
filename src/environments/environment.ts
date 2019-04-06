57a357fd6d209e781c69561125073df83ab854fc
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-04-06 17:18:48 -0300

    set firebase environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMko2hsgFjOSF-Lz-djTlVFdDY3Plq97A",
+    authDomain: "oshop-dea22.firebaseapp.com",
+    databaseURL: "https://oshop-dea22.firebaseio.com",
+    projectId: "oshop-dea22",
+    storageBucket: "oshop-dea22.appspot.com",
+    messagingSenderId: "61247301260"
+  }
 };
 
 /*
