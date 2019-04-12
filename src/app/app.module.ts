e4848ce5637db067f677c465302260b4a0c8677c
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-04-12 17:23:13 -0300

    add storing users in firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { AuthService } from './auth.service';
 import { AuthGuardService } from './auth-guard.service';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,7 @@ import { AuthGuardService } from './auth-guard.service';
       { path: "admin/orders", component: AdminOrdersComponent, canActivate: [AuthGuardService]},
     ])
   ],
-  providers: [AuthService, AuthGuardService],
+  providers: [AuthService, AuthGuardService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
